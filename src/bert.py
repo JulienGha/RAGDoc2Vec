d88bac1a1756d2fee53a0976f23573f7d4f83597
JulienGha <julien.ghali@efrei.net>
2023-11-22 13:55:42 +0900

    data cleaner

--- src/bert.py
+++ src/bert.py
@@ -31,24 +31,3 @@ def load_bert_model(path="../models/bert/bert_model.pkl"):
     with open(path, "rb") as f:
         encoded_docs = pickle.load(f)
     return encoded_docs
-
-
-# Function to retrieve documents using BERT encoded vectors
-def retrieve_documents_bert(query, encoded_docs, documents, topn=5):
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    model = BertModel.from_pretrained('bert-base-uncased')
-
-    # Encoding the query
-    encoded_query = tokenizer(query, padding=True, truncation=True, max_length=512, return_tensors='pt')
-    with torch.no_grad():
-        query_output = model(**encoded_query)
-    query_vector = query_output.last_hidden_state.mean(dim=1).squeeze().numpy()
-
-    # Calculating similarities
-    similarities = cosine_similarity([query_vector], encoded_docs).flatten()
-    related_doc_indices = similarities.argsort()[-topn:][::-1]
-
-    # Fetch the actual documents using the indices
-    related_documents = [(idx, documents[idx]) for idx in related_doc_indices]
-    print(related_documents)
-    return related_documents
--- src/prompt_optimizer.py
+++ src/prompt_optimizer.py
@@ -5,9 +5,22 @@ def prompt_opti(prompt):
     tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small")
     model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-small")
 
-    input_text = "Answer the following." + prompt
+    # Step 1: Optimize the query
+    input_text = f"Turn this query into a question. {prompt}"
     input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+    optimized_query = model.generate(input_ids, max_length=256, num_beams=5, no_repeat_ngram_size=2, temperature=0.7)
+    optimized_query_text = tokenizer.decode(optimized_query[0], skip_special_tokens=True)
 
-    outputs = model.generate(input_ids, max_length=256)
-    print(tokenizer.decode(outputs[0]))
-    return tokenizer.decode(outputs[0])
+    # Step 2: Generate an answer based on the optimized query
+    input_text2 = f"Answer the following question step by step. {optimized_query_text}"
+    input_ids2 = tokenizer(input_text2, return_tensors="pt").input_ids
+    answer = model.generate(input_ids2, max_length=256, num_beams=5, no_repeat_ngram_size=2, temperature=0.7)
+    answer_text = tokenizer.decode(answer[0], skip_special_tokens=True)
+
+    print(f"Original Prompt: {prompt}")
+    print(f"Optimized Query: {optimized_query_text}")
+    print(f"Generated Answer: {answer_text}")
+    return answer_text
+
+
+prompt_opti("define hallucination")
